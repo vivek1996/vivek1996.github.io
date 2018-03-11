33ee62f7d5e7af50daacf06c4ce5e56a5bc36dff
Vivek <mvivek3112@gmail.com>
2018-03-11 19:48:52 +0530

    gulp error handler

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-
+const errorHandler = require('gulp-error-handle');
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -16,6 +16,7 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
+        .pipe(errorHandler())
         .pipe(sass())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
