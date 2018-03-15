179a91ed90dcd5e089ffd31998f64d60208bb7ca
vivek1996 <mvivek3112@gmail.com>
2018-03-15 16:49:34 +0530

    minified everything & created docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 const errorHandler = require('gulp-error-handle');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+const imagemin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
+
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -15,11 +21,25 @@ gulp.task('serve', ['sass'], function() {
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
+    var plugins = [
+        autoprefixer(),
+        cssnano()
+    ];
     return gulp.src("app/scss/*.scss")
         .pipe(errorHandler())
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest("./docs/css"))
         .pipe(browserSync.stream());
 });
-
+gulp.task('img', () =>
+	gulp.src('./app/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./docs/img'))
+);
+gulp.task('html', function() {
+    return gulp.src('./app/')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('./docs/'));
+  });
 gulp.task('default', ['serve']);
