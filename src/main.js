32e792375363e009ab36a376572bac0f232c986f
vivek1996 <mvivek3112@gmail.com>
2018-08-27 13:03:58 +0530

    removed mdb

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import Vue from "vue";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdbvue/build/css/mdb.css';
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
