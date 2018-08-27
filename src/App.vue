9973e015244d72fe64e24068d9e4acbacdba9bd8
vivek1996 <mvivek3112@gmail.com>
2018-08-27 10:59:25 +0530

    Removed Project Using Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,29 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      absolute
-      temporary
-      app
-      dark
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.link"
-        >
-          <v-list-tile-action>
-            <v-icon class="pink--text" v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class=" pink--text font-weight-bold" v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-
-    <v-footer app>
-       <v-flex
-        lighten-2
-        py-3
-        text-xs-center
-        white--text
-        xs12
-      >
-      With <v-icon class="pink--text" v-html="icons.favorite"></v-icon>
-        from Vivek M <v-icon class="pink--text" v-html="icons.face"></v-icon>
-      </v-flex>
-    </v-footer>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      items: [{
-        icon: 'home',
-        title: 'Home',
-        link: '/'
-      },
-      {
-        icon: 'book',
-        title: 'Projects',
-        link: '/projects'
-      },
-      {
-        icon: 'account_circle',
-        title: 'About',
-        link: '/about'
-      }
-      ],
-      miniVariant: false,
-      //  right: true,
-      // rightDrawer: false,
-      title: 'Vivek',
-      clipped: true,
-      icons: {
-        favorite: "favorite",
-        face: "mood"
-      }
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
-}
-</script>
-
-<style lang="scss">
-.application {
-  font-family: 'Montserrat', sans-serif !important;
-}
-.v-list__tile--active .v-list__tile__action i {
-  color: white;
-}
-.v-list__tile--active {
-  background-color: rgb(223, 223, 223);
 }
 </style>
