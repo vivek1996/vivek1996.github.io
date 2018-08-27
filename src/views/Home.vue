5bb977a5adad077d43018797d7ff1f28e143f02c
vivek1996 <mvivek3112@gmail.com>
2018-08-27 15:57:45 +0530

    hero section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <section class="hero is-fullheight is-success">
+  <div class="hero-head">
+
+  </div>
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Title
+      </h1>
+      <h2 class="subtitle">
+        Subtitle
+      </h2>
+    </div>
+  </div>
+</section>
   </div>
 </template>
 
