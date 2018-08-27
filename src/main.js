3c58f0040a803397d4d20d0d598dbaf234b16f57
vivek1996 <mvivek3112@gmail.com>
2018-08-27 11:46:04 +0530

    Added mdb

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbvue/build/css/mdb.css';
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
