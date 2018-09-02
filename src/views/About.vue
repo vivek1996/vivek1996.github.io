fb5c329c4a0407f9da915b33fa90517c0f1c4759
vivek1996 <mvivek3112@gmail.com>
2018-09-02 21:15:49 +0530

    Added social media links

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,25 +12,54 @@
             <strong class="has-text-warning"> {{name}} </strong>
             <br>
             <p> {{desc}} </p>
+            <p class="has-text-warning">
+              <i class="fas fa-envelope "></i>
+              Email:
+              <a href="mailto:mvivek3112@gmail.com" target="blank" class="has-text-warning">mvivek3112@gmail.com</a>
+            </p>
           </div>
         </div>
         <nav class="level is-mobile">
           <div class="level-left">
-            <a class="level-item">
-              <span class="icon is-small">
-                <i class="fas fa-reply"></i>
+
+            <a class="level-item hvr-icon-spin" href="https://www.facebook.com/vivek.udaya" target="blank" rel="noopener">
+              <span class="icon">
+                <i class="fab fa-facebook-f hvr-icon"></i>
               </span>
             </a>
-            <a class="level-item">
-              <span class="icon is-small">
-                <i class="fas fa-retweet"></i>
+            <a class="level-item hvr-icon-spin" href="https://twitter.com/VivekM31" target="blank" rel="noopener">
+              <span class="icon">
+                <i class="fab fa-twitter hvr-icon"></i>
               </span>
             </a>
-            <a class="level-item">
-              <span class="icon is-small">
-                <i class="fas fa-heart"></i>
+            <a class="level-item hvr-icon-spin" href="https://github.com/vivek1996" target="blank" rel="noopener">
+              <span class="icon">
+                <i class="fab fa-github hvr-icon"></i>
               </span>
             </a>
+            <a class="level-item hvr-icon-spin" href="https://www.linkedin.com/in/vivek-m-6598b1b7" target="blank" rel="noopener">
+              <span class="icon">
+                <i class="fab fa-linkedin-in hvr-icon"></i>
+              </span>
+            </a>
+            <a class="level-item hvr-icon-spin" href="https://www.instagram.com/vivek_m/" target="blank" rel="noopener">
+              <span class="icon">
+                <i class="fab fa-instagram hvr-icon"></i>
+              </span>
+            </a>
+            <a class="level-item hvr-icon-spin" href="https://medium.com/@mvivek3112" target="blank" rel="noopener">
+              <span class="icon">
+                <i class="fab fa-medium-m hvr-icon"></i>
+              </span>
+            </a>
+            <!-- Stack icon -->
+            <!-- <a class="level-item hvr-icon-spin" target="blank" rel="noopener">
+              <span class="fa-stack">
+                <i class="fas fa-circle fa-stack-2x" style="color:#fff"></i>
+                <i class="fab fa-instagram hvr-icon fa-stack-1x fa-inverse"></i>
+              </span>
+            </a> -->
+            <!-- / stack icon -->
           </div>
         </nav>
       </div>
@@ -67,6 +96,41 @@
 .media-content {
   flex: 99999 1 20em;
   max-width: 30em;
+}
+.fa-facebook-f {
+  color: #3b5998;
+}
+.fa-twitter {
+  color: #1da1f2;
+}
+.fa-github {
+  color: #4078c0;
+}
+.fa-linkedin-in {
+  color: #0077b5;
+}
+.fa-instagram {
+  color: #833ab4;
+}
+.fa-medium-m {
+  color: #00ab6c;
+}
+/* Icon Spin */
+.hvr-icon-spin {
+  display: inline-block;
+  vertical-align: middle;
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+}
+.hvr-icon-spin .hvr-icon {
+  transition-duration: 1s;
+  transition-property: transform;
+  transition-timing-function: ease-in-out;
+}
+.hvr-icon-spin:hover .hvr-icon,
+.hvr-icon-spin:focus .hvr-icon,
+.hvr-icon-spin:active .hvr-icon {
+  transform: rotate(360deg);
 }
 </style>
 <script>
