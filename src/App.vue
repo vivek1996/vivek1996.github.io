6a6c9b848338a3d6805039b1e294b9c1f92d6533
vivek1996 <mvivek3112@gmail.com>
2018-09-07 11:03:31 +0530

    Updated FontSize &  Font family

--- src/App.vue
+++ src/App.vue
@@ -16,20 +16,20 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700');
+// @import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700');
 // body {
 //   font-size: calc(
 //     14px + (26 - 14) * ((100vw - 300px) / (1600 - 300))
 //   ) !important;
 // }
-html {
-  font-size: calc(80% + 0.5vw) !important;
-}
+// html {
+//   font-size: calc(80% + 0.5vw) !important;
+// }
 #app {
   // background-color: hsl(0, 0%, 21%);
   background: #232526;
   background: linear-gradient(to right, #414345, #232526);
-  font-family: 'Noto Sans', sans-serif;
+ // font-family: 'Noto Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   //min-height: 85vh;
