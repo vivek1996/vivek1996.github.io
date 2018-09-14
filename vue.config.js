93b5a94dd2f1e07caa7a062ae2e2b693ff188e84
vivek1996 <mvivek3112@gmail.com>
2018-09-14 22:00:53 +0530

    renamed timeline

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-// const path = require("path");
-// const PrerenderSPAPlugin = require("prerender-spa-plugin");
+ const path = require("path");
+ const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
-// module.exports = {
-//   configureWebpack: {
-//     plugins: [
-//       new PrerenderSPAPlugin({
-//         staticDir: path.join(__dirname, "dist"),
-//         routes: ["/", "/about", "/projects"]
-//       })
-//     ]
-//   }
-// };
+ module.exports = {
+   configureWebpack: {
+     plugins: [
+       new PrerenderSPAPlugin({
+         staticDir: path.join(__dirname, "dist"),
+         routes: ["/", "/about", "/projects"]
+       })
+     ]
+   }
+ };
